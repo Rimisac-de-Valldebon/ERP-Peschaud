b9ca0a0c0297a34b67c81ba9cefce6b99ec4c213
Rimisac de Valldebon <casimir.debonneval@peschaud.fr>
2025-01-10 10:58:28 +0100

    Include Config File

---
+++ config.js
@@ -0,0 +1,14 @@
+const credentials = {
+    users: [
+        {
+            username: 'admin',
+            password: 'password'
+        },
+        {
+            username: 'user1',
+            password: 'pass1'
+        }
+    ]
+};
+
+module.exports = credentials; 
