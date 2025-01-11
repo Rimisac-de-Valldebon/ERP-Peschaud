a9e41b32f96a7e6f39d2b9bb6bf62905d0f9a8df
Rimisac de Valldebon <casimir.debonneval@peschaud.fr>
2025-01-11 16:35:51 +0100

    Adding security to block access to pages without logging in

--- config.js
+++ config.js
@@ -7,6 +7,18 @@ const credentials = {
         {
             username: 'user1',
             password: 'pass1'
+        },
+        {
+            username: 'user2',
+            password: 'pass2'
+        },
+        {
+            username: 'manager',
+            password: 'mpass123'
+        },
+        {
+            username: 'supervisor',
+            password: 'super456'
         }
     ]
 };
